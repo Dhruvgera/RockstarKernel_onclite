c13fb8025f630baa20b1b492eb45a9f8f96397ed
Dhruv <dhruvgera61@gmail.com>
2019-12-25 08:08:59 +0000

    Makefile: Fix Werror

--- unified.sh
+++ unified.sh
@@ -9,9 +9,9 @@ git config --global user.name "Dhruv"
 export TZ="Asia/Kolkata";
  
 # Kernel compiling script
-mkdir -p /home/TC
+mkdir -p $HOME/TC
 git clone https://github.com/Dhruvgera/AnyKernel3.git -b onclite
-git clone https://github.com/RaphielGang/aarch64-linux-gnu-8.x /home/TC/aarch64-linux-gnu-8.x --depth=1
+git clone https://github.com/Dhruvgera/Linaro_5.5.git  $HOME/TC/aarch64-linux-gnu-8.x --depth=1
 git clone https://github.com/VRanger/clang.git dragontc
 git clone -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/ "$HOME"/TC/gcc32 --depth=1 
 function check_toolchain() {
